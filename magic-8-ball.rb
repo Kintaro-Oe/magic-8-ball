8fb254d0b20616e9223ba3ea00f8d0015bc94c38
Kintaro-Oe <marcusclairmont@gmail.com>
2019-03-13 13:18:27 +0000

    Add magic-8-ball

---
+++ magic-8-ball.rb
@@ -0,0 +1,11 @@
+# Randomly choose one of five predictions:
+
+fortune = [
+  "It is certain",
+  "It is decidedly so",
+  "Ask again later",
+  "Outlook not so good",
+  "Very doubtful"
+  ]
+  
+puts fortune.sample
